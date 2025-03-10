1c1c253a3d8c36d52171f6e3b0cfd2961267f20a
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-10 07:44:28 -0300

    Eventos de domínio & cache - Integrando cache no Prisma

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -121,6 +121,7 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
       this.questionAttachmentsRepository.deleteMany(
         question.attachments.getRemovedItems(),
       ),
+      this.cache.delete(`question:${data.slug}:details`),
     ])
 
     DomainEvents.dispatchEventsForAggregate(question.id)
